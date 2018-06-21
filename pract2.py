a6e8e625fb2361daab4e53a7cec4e10cc5623c5c
dineshk813057 <40352058+dineshk813057@users.noreply.github.com>
2018-06-21 15:24:33 +0530

    Add files via upload

---
+++ pract2.py
@@ -0,0 +1,33 @@
+# question 2
+import datetime
+current=datetime.datetime.now()
+print(current.strftime("%Y %b %d"))
+
+
+# question 3
+import datetime
+month=datetime.datetime.now()
+print(month.stftime("%B"))
+
+#question 4
+import datetime
+day=datetime.datetime.now()
+print(day.strftime("%d"))
+
+#question 5
+import datetime
+y=datetime.datetime(2021,11,01)
+print(d.day)
+
+#question 7
+import math
+n=int(input("enter number"))
+print(math.factorial(n))
+
+#question 8
+import math
+n=int(input("enter first number"))
+m=int(input("enter second number"))
+print(math.gcd(n,m))
+
+
